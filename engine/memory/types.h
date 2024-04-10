200bb446bc0bb2edf5cf181b582bedef6a140e51
Matt Gallivan <gallivan.matt@gmail.com>
2024-04-09 22:12:52 -0600

    Add types

---
+++ engine/memory/types.h
@@ -0,0 +1,23 @@
+#ifdef TYPES_H
+#define TYPES_H
+
+#include <cstdint>
+
+namespace Cairn {
+
+using U8 = uint8_t;
+using U16 = uint16_t;
+using U32 = uint32_t;
+using U64 = uint64_t;
+
+using I8 = int8_t;
+using I16 = int16_t;
+using I32 = int32_t;
+using I64 = int64_t;
+
+using F32 = float;
+using F64 = double;
+
+}
+
+#endif // TYPES_H
