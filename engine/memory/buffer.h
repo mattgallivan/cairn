aaf3a9894d8df131449e521322151997d9eb0cdf
Matt Gallivan <gallivan.matt@gmail.com>
2024-04-11 23:41:18 -0600

    Remove default values when using constructor initialization in buffer

--- engine/memory/buffer.h
+++ engine/memory/buffer.h
@@ -8,13 +8,13 @@ namespace Cairn {
 class Buffer {
 
 public:
-  Buffer(Byte* data, Size size) : data(data), size(size){};
+  Buffer(Byte* data, Size size) : data(data), size(size), length(0){};
   Buffer(const Buffer& other) = default;
   Buffer& operator=(const Buffer& other) = default;
 
-  Byte* data = nullptr;
-  Size size = 0;
-  Size length = 0;
+  Byte* data;
+  Size size;
+  Size length;
 };
 
 } // namespace Cairn
