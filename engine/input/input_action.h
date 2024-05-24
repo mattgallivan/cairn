157646c1450af3611057b759b0fa51ee788c70f2
Matt Gallivan <gallivan.matt@gmail.com>
2024-05-23 21:13:46 -0600

    Finalize input code

--- engine/input/input_action.h
+++ engine/input/input_action.h
@@ -6,6 +6,7 @@
 
 namespace Cairn {
 
+/** An input action is a callback that is called when an input event is received. */
 using InputAction = std::function<void(const InputEvent&)>;
 
 } // namespace Cairn
