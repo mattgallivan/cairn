e6fb1211ec3830b566e99b52bf284bd4acbb2d83
Matt Gallivan <gallivan.matt@gmail.com>
2024-06-01 18:49:22 -0600

    Add default constructor for color and add more predefined colors

--- engine/ui/ui_element.h
+++ engine/ui/ui_element.h
@@ -5,14 +5,25 @@
 namespace Cairn {
 
 struct Color {
+  /** Construct a color with the default RGBA values (black). */
+  Color() : r(0.0f), g(0.0f), b(0.0f), a(1.0f) {}
+
+  /** Construct a color with the given RGBA values. */
   Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {}
 
   float r, g, b, a;
 };
 
+// Predefined colors.
 static const Color Black = {0.0f, 0.0f, 0.0f, 1.0f};
 static const Color White = {1.0f, 1.0f, 1.0f, 1.0f};
 static const Color Red = {1.0f, 0.0f, 0.0f, 1.0f};
+static const Color Green = {0.0f, 1.0f, 0.0f, 1.0f};
+static const Color Blue = {0.0f, 0.0f, 1.0f, 1.0f};
+static const Color Yellow = {1.0f, 1.0f, 0.0f, 1.0f};
+static const Color Cyan = {0.0f, 1.0f, 1.0f, 1.0f};
+static const Color Magenta = {1.0f, 0.0f, 1.0f, 1.0f};
+static const Color Gray = {0.5f, 0.5f, 0.5f, 1.0f};
 
 /** A UI element is an abstract class that represents a UI element that is drawn on the screen. */
 class UIElement {
