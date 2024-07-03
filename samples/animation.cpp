d2be3fa287d93f466a1a98c64b271d7f850964b4
Matt Gallivan <gallivan.matt@gmail.com>
2024-07-03 15:21:21 -0600

    Add runnable samples for testing an animation-independent visual example

--- samples/animation.cpp
+++ samples/animation.cpp
@@ -1,18 +1,6 @@
-#include "window.h"
+#include <iostream>
 
 int main() {
-  Cairn::Window window(800, 600, "Animation Test");
-
-  Cairn::InputManager input_manager;
-  Cairn::InputProfile profile;
-  profile.map(Cairn::KeyboardEvent(Cairn::KeyboardEvent::Key::Escape, Cairn::InputEvent::State::Press),
-              [&window](const Cairn::InputEvent& event) { window.close(); });
-  input_manager.add(&profile);
-  window.bind(&input_manager);
-
-  while (window.is_open()) {
-    window.refresh();
-  }
-
+  std::cout << "Hello, World!" << std::endl;
   return 0;
 }
